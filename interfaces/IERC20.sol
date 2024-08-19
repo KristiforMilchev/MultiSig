06d8108bad0bbba957a8145a8428f2185232b9e8
Kristifor Milchev <km@airzen.io>
2024-08-19 12:55:05 +0300

    Adding the decimals function as external view for the IERC20 interface.

--- interfaces/IERC20.sol
+++ interfaces/IERC20.sol
@@ -19,6 +19,8 @@ interface IERC20 {
 
     function approve(address spender, uint256 amount) external returns (bool);
 
+    function decimals() external view returns (uint8);
+
     function transferFrom(
         address sender,
         address recipient,
