3dae1c1605c5e46c5995d54a0f934ef3d039d8a4
Kristifor Milchev <km@airzen.io>
2024-08-28 15:03:35 +0300

    Implementing USD conversion.

--- contracts/FeeService.sol
+++ contracts/FeeService.sol
@@ -4,25 +4,14 @@ pragma solidity ^0.8.19;
 import "./../interfaces/AggregatorV3.sol";
 
 contract FeeService {
-    AggregatorV3Interface internal priceFeed; // Declare priceFeed interface
-    uint256 private feeModifier;
+    AggregatorV3Interface internal priceFeed;
+    uint256 private feeInUsd;
     address private taxAddress;
-    mapping(address => bool) public tokenExists;
 
-    constructor(address _priceFeed, address _taxAddress, uint256 _fee) {
+    constructor(address _priceFeed, address _taxAddress, uint256 _feeInUsd) {
         priceFeed = AggregatorV3Interface(_priceFeed);
-        feeModifier = _fee;
-        _taxAddress = taxAddress;
-    }
-
-    function getRegistrationFee() public view returns (uint256) {
-        int price = getLatestPrice();
-        require(price > 0, "Invalid price from price feed");
-
-        uint256 priceInUsd = uint256(price);
-        uint256 weiPerUsd = (1 * 10 ** 18) / priceInUsd;
-
-        return weiPerUsd;
+        feeInUsd = _feeInUsd;
+        taxAddress = _taxAddress;
     }
 
     function getLatestPrice() private view returns (int) {
@@ -30,12 +19,25 @@ contract FeeService {
         return price;
     }
 
-    function tokenRegistrationFee() public view returns (uint256) {
-        return getRegistrationFee() * feeModifier;
+    function getFeeInEthAndUsd()
+        public
+        view
+        returns (uint256 feeInWei, uint256 feeInUsdValue)
+    {
+        int price = getLatestPrice();
+        require(price > 0, "Invalid price from price feed");
+
+        uint256 priceInUsd = uint256(price);
+        uint256 weiPerUsd = (1 * 10 ** 18) / priceInUsd;
+        feeInWei = feeInUsd * weiPerUsd;
+        feeInUsdValue = feeInUsd * 1e18;
+        return (feeInWei, feeInUsdValue);
     }
 
-    function changeTax(uint256 tax) public payable onlyOwner returns (bool) {
-        feeModifier = tax;
+    function changeTax(
+        uint256 newFeeInUsd
+    ) public payable onlyOwner returns (bool) {
+        feeInUsd = newFeeInUsd;
         return true;
     }
 
