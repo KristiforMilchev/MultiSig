531f059f17a84e0054141b18bf2e84b140b3f2ad
Kristifor Milchev <km@airzen.io>
2024-10-01 15:35:14 +0300

    adding unit tests for the fee service.

--- test/fee_service.js
+++ test/fee_service.js
@@ -1,13 +1,15 @@
 const FeeService = artifacts.require("FeeService");
-const MockAggregatorV3 = artifacts.require("AggregatorV3");
 const { getNonce } = require("./../utils/helpers");
+const assert = require("assert");
+const MockAggregatorV3 = require("./mocks/aggregator_v3_interface");
 
 contract("FeeService", function (accounts) {
   let feeService;
   let mockAggregator;
 
   before(async () => {
-    mockAggregator = await MockAggregatorV3.new();
+    mockAggregator = await MockAggregatorV3.instance(accounts[0]);
+
     feeService = await FeeService.new(mockAggregator.address, accounts[0], 10);
   });
 
@@ -17,15 +19,28 @@ contract("FeeService", function (accounts) {
     const feeInWei = result.feeInWei.toString();
     const feeInUsdValue = result.feeInUsdValue.toString();
 
-    console.log("Fee in Wei:", feeInWei);
-    console.log("Fee in USD (in Wei units):", feeInUsdValue);
-
-    assert.isTrue(feeInWei > 0, "Fee in Wei should be greater than zero");
+    assert.equal(feeInWei > 0, true, "Fee in Wei should be greater than zero");
     assert.equal(
       feeInUsdValue,
       web3.utils.toWei("10", "ether"),
       "Fee in USD should be 10 USD in Wei units"
     );
+  });
+  it("Should convert USD to Wei", async () => {
+    try {
+      const amount = await feeService.convertUsdToWei(18);
+      const ethPrice = 1800 * 1e8;
+      const expectedWei = Math.floor((18 * 1e18) / ethPrice);
+
+      assert.equal(
+        amount.toString(),
+        expectedWei.toString(),
+        "The converted amount should be correct"
+      );
+    } catch (ex) {
+      console.log(ex);
+      assert.fail("Failed to retrive usd in wei");
+    }
   });
 
   it("should update the fee correctly", async () => {
@@ -40,6 +55,11 @@ contract("FeeService", function (accounts) {
       web3.utils.toWei("20", "ether"),
       "Fee in USD should be 20 USD in Wei units"
     );
+  });
+
+  it("Should return price greater than zero at the current block", async () => {
+    const price = await feeService.getLatestPrice();
+    assert.notEqual(price, 0, "Price returned from price feed is 0");
   });
 
   // it("should deny others than the deployer to update fees", async () => {
