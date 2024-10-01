2a44dd4a7e3c0d6ae87013132afb551d49f597c7
Kristifor Milchev <km@airzen.io>
2024-10-01 15:33:32 +0300

    Separating the fee service from the contract service.

--- contracts/FeeService.sol
+++ contracts/FeeService.sol
@@ -12,23 +12,10 @@ contract FeeService is IPriceFeed {
     AggregatorV3Interface internal priceFeed;
     uint256 private feeInUsd;
     address private taxAddress;
-    mapping(string => Factory) public factories;
-
-    constructor(
-        address _priceFeed,
-        address _taxAddress,
-        uint256 _feeInUsd,
-        address _factory,
-        address _networkWrappedToken,
-        string memory _defaultFactoryName
-    ) {
+    constructor(address _priceFeed, address _taxAddress, uint256 _feeInUsd) {
         priceFeed = AggregatorV3Interface(_priceFeed);
         feeInUsd = _feeInUsd;
         taxAddress = _taxAddress;
-        factories[_defaultFactoryName] = Factory({
-            at: _factory,
-            wth: _networkWrappedToken
-        });
     }
 
     function getFeeInEthAndUsd()
@@ -51,73 +38,6 @@ contract FeeService is IPriceFeed {
         return price;
     }
 
-    function getPairAddress(
-        address tokenA,
-        address tokenB,
-        string memory factoryName
-    ) public view returns (address pairAddress) {
-        return IV2Factory(factories[factoryName].at).getPair(tokenA, tokenB);
-    }
-
-    function getPairForTokens(
-        address tokenA,
-        string memory _name
-    ) public view returns (address) {
-        Factory storage currentFactory = factories[_name];
-        IV2Factory factory = IV2Factory(currentFactory.at);
-        return factory.getPair(tokenA, currentFactory.wth);
-    }
-
-    function getTokenReserves(
-        ITokenPairV2 pair,
-        address token
-    ) internal view returns (address tokenReserve, address stablecoinReserve) {
-        address token0 = pair.token0();
-        address token1 = pair.token1();
-
-        return (token0 == token) ? (token0, token1) : (token1, token0);
-    }
-
-    function getReserveAmounts(
-        ITokenPairV2 pair,
-        address tokenReserve
-    )
-        internal
-        view
-        returns (uint112 tokenReserveAmount, uint112 stablecoinReserveAmount)
-    {
-        (uint112 reserve0, uint112 reserve1, ) = pair.getReserves();
-        address token0 = pair.token0();
-
-        return
-            (tokenReserve == token0)
-                ? (reserve0, reserve1)
-                : (reserve1, reserve0);
-    }
-
-    function calculatePriceInUsd(
-        uint112 tokenReserveAmount,
-        uint112 stablecoinReserveAmount,
-        uint8 tokenDecimals,
-        uint8 stablecoinDecimals
-    ) internal pure returns (uint256) {
-        uint256 tokenReserveNormalized = uint256(tokenReserveAmount) *
-            10 ** uint256(stablecoinDecimals);
-        uint256 stablecoinReserveNormalized = uint256(stablecoinReserveAmount) *
-            10 ** uint256(tokenDecimals);
-
-        return (stablecoinReserveNormalized * 1e18) / tokenReserveNormalized;
-    }
-
-    function calculateTokenAmount(
-        uint256 usdAmount,
-        uint256 tokenPriceInUsd,
-        uint8 tokenDecimals
-    ) internal pure returns (uint256) {
-        return
-            (usdAmount * 10 ** uint256(tokenDecimals) * 1e18) / tokenPriceInUsd;
-    }
-
     function convertUsdToWei(uint256 usdAmount) public view returns (uint256) {
         (, int ethToUsdPrice, , , ) = priceFeed.latestRoundData();
 
@@ -128,44 +48,6 @@ contract FeeService is IPriceFeed {
         return etherAmount;
     }
 
-    function convertUsdToTokenWei(
-        address token,
-        uint256 usdAmount,
-        string memory factory
-    ) public view returns (uint256) {
-        address pairAddress = getPairForTokens(token, factory);
-        require(pairAddress != address(0), "Pair not found");
-
-        ITokenPairV2 pair = ITokenPairV2(pairAddress);
-        (address tokenReserve, address stablecoinReserve) = getTokenReserves(
-            pair,
-            token
-        );
-
-        (
-            uint112 tokenReserveAmount,
-            uint112 stablecoinReserveAmount
-        ) = getReserveAmounts(pair, tokenReserve);
-
-        uint8 tokenDecimals = IERC20(tokenReserve).decimals();
-        uint8 stablecoinDecimals = IERC20(stablecoinReserve).decimals();
-
-        uint256 tokenPriceInUsd = calculatePriceInUsd(
-            tokenReserveAmount,
-            stablecoinReserveAmount,
-            tokenDecimals,
-            stablecoinDecimals
-        );
-
-        uint256 tokenAmountInWei = calculateTokenAmount(
-            usdAmount,
-            tokenPriceInUsd,
-            tokenDecimals
-        );
-
-        return tokenAmountInWei;
-    }
-
     function convertWeiToUsd(
         uint256 amountInWei
     ) public view returns (uint256) {
@@ -173,37 +55,6 @@ contract FeeService is IPriceFeed {
 
         require(ethToUsdPrice > 0, "Invalid ETH/USD price");
         return (amountInWei * uint256(ethToUsdPrice)) / 1e18;
-    }
-
-    function convertTokenToUsd(
-        address token,
-        uint256 amount,
-        string memory factory
-    ) public view returns (uint256) {
-        address pairAddress = getPairForTokens(token, factory);
-        require(pairAddress != address(0), "Pair not found");
-
-        ITokenPairV2 pair = ITokenPairV2(pairAddress);
-        (address tokenReserve, address stablecoinReserve) = getTokenReserves(
-            pair,
-            token
-        );
-        (
-            uint112 tokenReserveAmount,
-            uint112 stablecoinReserveAmount
-        ) = getReserveAmounts(pair, tokenReserve);
-
-        uint8 tokenDecimals = IERC20(tokenReserve).decimals();
-        uint8 stablecoinDecimals = IERC20(stablecoinReserve).decimals();
-
-        uint256 tokenPriceInUsd = calculatePriceInUsd(
-            tokenReserveAmount,
-            stablecoinReserveAmount,
-            tokenDecimals,
-            stablecoinDecimals
-        );
-
-        return (amount * tokenPriceInUsd) / 10 ** tokenDecimals;
     }
 
     function changeTax(uint256 newFeeInUsd) external onlyOwner returns (bool) {
--- interfaces/IPriceFeed.sol
+++ interfaces/IPriceFeed.sol
@@ -13,26 +13,10 @@ interface IPriceFeed {
         view
         returns (uint256 feeInWei, uint256 feeInUsdValue);
 
-    function getPairForTokens(
-        address tokenA,
-        string memory _name
-    ) external view returns (address);
     function convertUsdToWei(uint256 usdAmount) external view returns (uint256);
-
-    function convertUsdToTokenWei(
-        address token,
-        uint256 usdAmount,
-        string memory factory
-    ) external view returns (uint256);
 
     function convertWeiToUsd(
         uint256 amountInWei
-    ) external view returns (uint256);
-
-    function convertTokenToUsd(
-        address token,
-        uint256 amount,
-        string memory factory
     ) external view returns (uint256);
 
     function changeTax(uint256 newFeeInUsd) external returns (bool);
