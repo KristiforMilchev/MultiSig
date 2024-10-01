861bd98390c42434d232390d7e4c72d849fcefb6
Kristifor Milchev <km@airzen.io>
2024-10-01 17:35:35 +0300

    adding a mock for the fee service.

--- test/mocks/fee_service_mock.js
+++ test/mocks/fee_service_mock.js
@@ -1,10 +1,28 @@
-class FeeServiceMock {
-  getFeeInEthAndUsd() {
-    const feeInWei = 900000000000;
-    const usd = 100;
+const feeService = artifacts.require("FeeService");
+const { deployMockContract } = require("@ethereum-waffle/mock-contract");
+const { getSigner } = require("./../../utils/helpers");
 
-    return [feeInWei, usd];
-  }
+async function instance(account) {
+  let signer = await getSigner(account);
+  let mockFeeService = await deployMockContract(signer, feeService.abi);
+
+  // Mocking the return values
+  await mockFeeService.mock.getFeeInEthAndUsd.returns(
+    BigInt(900000000000), // feeInWei
+    BigInt(100) // feeInUsdValue
+  );
+  await mockFeeService.mock.getLatestPrice.returns(9000000000);
+  await mockFeeService.mock.convertUsdToWei
+    .withArgs(100)
+    .returns(BigInt(900000000000));
+  await mockFeeService.mock.convertWeiToUsd
+    .withArgs(BigInt(900000000000))
+    .returns(100);
+  await mockFeeService.mock.changeTax.returns(true);
+
+  return mockFeeService;
 }
 
-module.exports = FeeServiceMock;
+module.exports = {
+  instance,
+};
