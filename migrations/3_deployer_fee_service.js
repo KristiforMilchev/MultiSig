bdd31895cb9ddbc6c5ee85c16a480290dff679fb
Kristifor Milchev <km@airzen.io>
2024-11-01 10:02:14 +0200

    Refactoring the migration files, removing the dpendency on the real implementation of the AggregatorV3Interface to make testing easier.

---
+++ migrations/3_deployer_fee_service.js
@@ -0,0 +1,25 @@
+const FeeService = artifacts.require("FeeService");
+require("dotenv").config();
+
+const aggregatorV3 = "AggregatorV3";
+const AggregatorV3 = artifacts.require(aggregatorV3);
+module.exports = async function (deployer) {
+  console.log(process.env.PKSwapFactory);
+  console.log(process.env.FeeCollectorAddress);
+
+  const networkId = await web3.eth.net.getId();
+  const deployedAggregator = AggregatorV3.networks[networkId];
+  if (!deployedAggregator) {
+    throw new Error(`Contract not found on network with ID ${networkId}`);
+  }
+  const deployedAggregatorServiceAddress = deployedAggregator.address;
+  console.log("Aggregator Service address");
+  console.log(deployedAggregatorServiceAddress);
+
+  await deployer.deploy(
+    FeeService,
+    deployedAggregatorServiceAddress,
+    process.env.FeeCollectorAddress,
+    100
+  );
+};
--- migrations/4_deployer_payment_factory.js
+++ migrations/4_deployer_payment_factory.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 
 const feeService = "FeeService";
 const feeServiceContract = artifacts.require(feeService);
+
+const aggregatorV3 = "AggregatorV3";
+const AggregatorV3 = artifacts.require(aggregatorV3);
 module.exports = async function (deployer) {
   const networkId = await web3.eth.net.getId();
   const deployedFeeService = feeServiceContract.networks[networkId];
@@ -13,13 +16,18 @@ module.exports = async function (deployer) {
   console.log("Fee Service address");
   console.log(deployedFeeServiceAddress);
 
+  const deployedAggregator = AggregatorV3.networks[networkId];
+  if (!deployedAggregator) {
+    throw new Error(`Contract not found on network with ID ${networkId}`);
+  }
+  const deployedAggregatorServiceAddress = deployedAggregator.address;
+  console.log("Aggregator Service address");
+  console.log(deployedAggregatorServiceAddress);
+
   await deployer.deploy(
     MsFactory,
     deployedFeeServiceAddress, // FeeService address used to calculate factory tax in WETH
-    // The address of AggregatorV3Interface deployed on my testnet,
-    // for other networks you should find the address and replace it
-    //(it's reposible for managing price updates when converting units inside the contract.)
-    process.env.AggregatorV3Interface,
+    deployedAggregatorServiceAddress,
     process.env.PKSwapFactory, // Factory for the PKSWAP deployed on my testnet,
     process.env.WrappedToken, // Network wrapped token
     "PkSwap",
